d668771fc1c7f7fef610d12437c8c636d9bd6717
PUNITH KUMAR <215137747+elitepunith@users.noreply.github.com>
2026-04-30 11:53:27 +0530

    updated

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ This is a basic website about the Mariana trench , this website has all the info
 
 1. HTML
 2. CSS
+3. javascript
+
+### Features
+
+1. This website have collection of deep sea creatures and information about that !
+
+2. This website contains images of deep - creatures human know !
+
+3. This website also have a music play button , so you can read while lisenting ocean music ! 
 
 ### Preview
 
--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function flipCard(card) {
-  
-  const inner = card.querySelector('.flipcard-post');
-  
-  inner.classList.toggle('is-flipped');
+    const inner = card.querySelector('.flipcard-post');
+    inner.classList.toggle('is-flipped');
+}
+
+function toggleMusic() {
+    const audio = document.getElementById('ocean-audio');
+    const button = document.getElementById('music-toggle');
+
+    if (audio.paused) {
+        audio.play();
+        button.innerText = "🔇 Pause Music";
+    } else {
+        audio.pause();
+        button.innerText = "🔊 Play Music";
+    }
 }
